38ad8d8c8edac9f0382ffb72e937753978fd4d3a
Carl Green <carl.green@gronare.se>
2019-10-29 14:08:12 +0100

    adds keyword arguments to initializer to fix deprecated positional arguments

--- lib/fake_braintree/server.rb
+++ lib/fake_braintree/server.rb
@@ -10,6 +10,8 @@ class FakeBraintree::Server
 
   def initialize(options = {})
     app = FakeBraintree::SinatraApp
-    @server = Capybara::Server.new(app, options.fetch(:port, nil), SERVER_HOST)
+    @server = Capybara::Server.new(app,
+                                   port: options.fetch(:port, nil),
+                                   host: SERVER_HOST)
   end
 end
